541bd71491e66a9d976c11482a484b9ae813dfde
dannyirwin <31043298+dannyirwin@users.noreply.github.com>
2020-09-01 12:49:58 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,45 @@
+/*function scrollFunction() {
+    let name = document.getElementById("name");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        name.classList.remove("scroll-top");
+        name.classlist.add("scroll-not-top");
+    } else {
+        name.classList.remove("scroll-not-top");
+        name.classList.add("scroll-top");
+    }
+}*/
+
+function growLine() {
+    let lines = document.querySelectorAll(".line");
+    for (let i in lines) {
+
+        let line = lines[i];
+        line.style.width = "100%";
+    }
+}
+
+/*
+window.onscroll = function () {
+    scrollFunction();
+};
+*/
+
+let readMoreButtonArr = document.getElementsByClassName("readMoreButton");
+
+for (let i = 0; i < readMoreButtonArr.length; i++) {
+    readMoreButtonArr[i].addEventListener("click", function () {
+        var content = this.parentElement.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+            this.innerHTML = "Read More";
+        } else {
+            content.style.maxHeight = content.scrollHeight + 10 + "px";
+            this.innerHTML = "Read Less";
+        }
+    });
+}
+
+
+    
+
+growLine();
