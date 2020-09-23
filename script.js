a69413c5a6cfb357cc00745ba53bc2b064d50ab9
dannyirwin <31043298+dannyirwin@users.noreply.github.com>
2020-09-23 14:10:09 -0600

    edits from shira 2

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 let readMoreButtonArr = document.getElementsByClassName("readMoreButton");
 
 function growLine() {
-    const lines = document.querySelectorAll(".line");
-    for (let i in lines) {
-
-        const line = lines[i];
-        line.style.width = "100%";
-    }
+	const lines = document.querySelectorAll(".line");
+	for (let i in lines) {
+		let line = lines[i];
+		line.style.width = "100%";
+	}
 }
 
-for (let i = 0; i < readMoreButtonArr.length; i++) { // put in onload or ready function
-    readMoreButtonArr[i].addEventListener("click", () => {
-        const content = this.parentElement.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-            this.firstElementChild.innerHTML = "Read More";
-        } else {
-            content.style.maxHeight = content.scrollHeight + 10 + "px";
-            this.firstElementChild.innerHTML = "Read Less";
-        }
-	});
+function addReadMoreEventListeners() {
+	for (let i = 0; i < readMoreButtonArr.length; i++) {
+		readMoreButtonArr[i].addEventListener("click", function () {
+			let content = this.parentElement.nextElementSibling;
+			for (let i = 0; i < readMoreButtonArr.length; i++) { // put in onload or ready function
+				readMoreButtonArr[i].addEventListener("click", () => {
+					const content = this.parentElement.nextElementSibling;
+
+					if (content.style.maxHeight) {
+						content.style.maxHeight = null;
+						this.firstElementChild.innerHTML = "Read More";
+					} else {
+						content.style.maxHeight = content.scrollHeight + 10 + "px";
+						this.firstElementChild.innerHTML = "Read Less";
+					}
+				});
+			}
+		});
+	}
 }
 
-growLine();
+window.addEventListener('load', (event) => {
+	addReadMoreEventListeners();
+	growLine();
+});
